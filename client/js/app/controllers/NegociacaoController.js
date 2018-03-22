65b5bd666dd5ed479e0d40d90a296109a69040ef
matheus solidade <mhsolidade@gmail.com>
2018-03-22 17:17:04 -0300

    refatorando a NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,35 +6,34 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
     
     adiciona(event){
         event.preventDefault();
 
+        this._listaNegociacoes.adiciona(this._criandoNegociacao());
+        this._liparCampos();
         
-        
-        let negociacao = new Negociacao(
+    }
+    
+    _liparCampos() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
+
+        this._inputData.focus();
+    }
+
+    _criandoNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade,
             this._inputValor
         );
-        
-        let diaMesAno = DateHelper.dataParaTexto(negociacao.data);
-        console.log(diaMesAno);
-        this.liparCampos();
-        
-
     }
-    liparCampos() {
-
-        this._inputData.value = '';
-            this._inputQuantidade.value = 1;
-            this._inputValor.value = 0;
-
-            this._inputData.focus();
-    }
-
 
 
 }
