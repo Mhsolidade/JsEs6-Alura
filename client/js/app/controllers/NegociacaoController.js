51cb14cb4d938dced47d0e1f72ebe34960b36020
matheus solidade <mhsolidade@gmail.com>
2018-03-21 22:58:46 -0300

    pegando o conteudo dos inputs

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,17 @@ class NegociacaoController {
 
     adiciona(event){
         event.preventDefault();
-        alert("Controller negociação")
+        
+        let $ = document.querySelector.bind(document);
+
+        
+        let inputData = $("#data");
+        let inputQuantidade = $("#quantidade");
+        let inputValor = $("#valor");
+
+        console.log(inputData);
+
+
 
     }
 
