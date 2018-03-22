c1c7098eca49af6f5742fa7254907fe54134f795
matheus solidade <mhsolidade@gmail.com>
2018-03-22 20:37:31 -0300

    adicionando o tatal de volume

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -31,6 +31,10 @@ class NegociacoesView {
                         `
                     }).join('')}
                 <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negociacoes.reduce((total,n)  => total + n.volume, 0.0)}
+                </td>
                 </tfoot>
             </table>
         `;
