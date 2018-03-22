9967289caf00134e6bc49866934aca5a347c439e
matheus solidade <mhsolidade@gmail.com>
2018-03-22 15:19:12 -0300

    Adicionando construtor e deixando os methos static em DeteHelper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,15 +12,15 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
 
-        let helper = new DateHelper();
+        
         
         let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade,
             this._inputValor
         );
         
-        let diaMesAno = helper.dataParaTexto(negociacao.data);
+        let diaMesAno = DateHelper.dataParaTexto(negociacao.data);
         console.log(diaMesAno);
         this.liparCampos();
         
